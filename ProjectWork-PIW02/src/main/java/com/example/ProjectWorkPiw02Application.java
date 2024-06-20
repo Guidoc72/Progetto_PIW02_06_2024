1ac7c1d0640fb51d0167ecbf443fdc1845f4a1de
Dev1 <fdk1997@gmail.com>
2024-06-20 16:30:07 +0200

    "classe utente"

--- ProjectWork-PIW02/src/main/java/com/example/ProjectWorkPiw02Application.java
+++ ProjectWork-PIW02/src/main/java/com/example/ProjectWorkPiw02Application.java
@@ -8,11 +8,7 @@ public class ProjectWorkPiw02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWorkPiw02Application.class, args);
-<<<<<<< HEAD
-		
-		
-=======
->>>>>>> c9b906665329dceaa4af6c7b6aa7da959ae34c92
+
 
 	}
 
