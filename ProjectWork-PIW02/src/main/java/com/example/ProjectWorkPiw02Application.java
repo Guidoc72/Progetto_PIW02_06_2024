26bfdc1969a144062e07f9bae7b6e129f8fa6f65
Dev1 <fdk1997@gmail.com>
2024-06-20 15:47:22 +0200

    sad

--- ProjectWork-PIW02/src/main/java/com/example/ProjectWorkPiw02Application.java
+++ ProjectWork-PIW02/src/main/java/com/example/ProjectWorkPiw02Application.java
@@ -8,14 +8,9 @@ public class ProjectWorkPiw02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWorkPiw02Application.class, args);
-<<<<<<< HEAD
 		
 		
-		//commento prova
-=======
 
-
->>>>>>> aa0bef6fb4411d923e4902de177068514f987869
 	}
 
 }
